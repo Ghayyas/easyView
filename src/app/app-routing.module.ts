ce0013c994e9b3d528e062012674fc360939a4d2
Ghayyas <ghayyasmubashir@gmail.com>
2019-04-07 00:04:31 +0500

    3 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' },
+  { path: 'room', loadChildren: './room/room.module#RoomPageModule' },
 ];
 
 @NgModule({
